57997bffca97399518d43cd9ef46c08d059d1a82
Elcio Cestari Taira <elciotaira@gmail.com>
2019-04-01 20:35:24 -0400

    Agora os fragments estão dinamicas. Ao clicar em um item da lista ele muda automaticamente o detalhes

--- app/src/main/java/com/elcio/workout5/MainActivity.java
+++ app/src/main/java/com/elcio/workout5/MainActivity.java
@@ -1,9 +1,11 @@
 package com.elcio.workout5;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Transition;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListner{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +14,18 @@ public class MainActivity extends AppCompatActivity {
 
         WorkoutDetailFragment detailFragment = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_detail);
 
-        detailFragment.setWorkoutId(0);
+        //detailFragment.setWorkoutId(0);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment detailFragment = new WorkoutDetailFragment();
+        FragmentTransaction transition = getSupportFragmentManager().beginTransaction();
+        detailFragment.setWorkoutId((int) id);
+        transition.replace(R.id.fragment_detail, detailFragment);
+        transition.addToBackStack(null);
+        transition.setTransition(transition.TRANSIT_FRAGMENT_FADE);
+        transition.commit();
+        //
     }
 }
-// vai mudar as coias agoras.
